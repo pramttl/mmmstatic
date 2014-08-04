3c5fbbc4900930da5ac90a0f790b02855d5bc983
Pranjal Mittal <mittal.pranjal@gmail.com>
2014-08-04 14:48:51 +0530

    Added static files required for new theme, bootstrap3, new font awesome.

--- js/mmm-utils.js
+++ js/mmm-utils.js
@@ -1,3 +1,8 @@
 function toggle(box) {
-    $(box).toggle()
+    $(box).toggle();
+    if (box=='#under_mmm_channel'){
+        $('#under_smtp_channel').toggle();
+    } else{
+        $('#under_mmm_channel').toggle();
+    }
 }
